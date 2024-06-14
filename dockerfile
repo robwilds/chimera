5237d27411727159bb17ba11bf3b40ae82f0c85f
rob wilds <robwilds@me.com>
2024-06-14 10:52:39 -0400

    label info being returned, need to refine angular app

---
+++ dockerfile
@@ -0,0 +1,14 @@
+FROM unit:python3.12
+WORKDIR /usr/share/nginx/html/python
+RUN apt-get update
+RUN apt-get install -y vim
+RUN apt install -y nginx
+RUN apt-get install -y systemd
+RUN service nginx stop && sleep 2
+COPY nginx.conf /etc/nginx/nginx.conf
+COPY /dist/alf-carousel/browser /usr/share/nginx/html
+COPY /python /usr/share/nginx/html/python
+RUN pip3 install -r requirements.txt
+COPY entrypoint.sh /
+ENTRYPOINT ["/bin/sh", "/entrypoint.sh"]
+#ENTRYPOINT python3 app.py && service nginx restart
---
+++ src/python/app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import python.getRekognitionFiles as getRekognitionFiles
+
+from flask import Flask,json,Response,request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route("/")
+def default():
+    return """<h1>Methods available:</h1>
+                <p>getrekognitionfiles</p>
+                <p>this will pull all images with aspect ai:labels</p>
+                <p>and place in ../src/assets/ (or ../assets/) folder for image carousel in</p>
+                """
+
+@app.route("/getrekognitionfiles")
+def getrekognitionfiles():
+    return Response(getRekognitionFiles.main().to_json(orient = 'records'),mimetype='text/json')
+
+if __name__ == "__main__":
+  # Please do not set debug=True in production
+  app.run(host="0.0.0.0", port=5202, debug=True)
