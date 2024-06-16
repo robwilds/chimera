5be928d9c567ca75df847da80cc4a30577ff9a77
Rob Wilds <robwilds@me.com>
2024-06-16 16:48:59 -0400

    dynamic images working and data to dialog.  need cleanup and reporting

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,18 @@
-import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, Inject,OnInit, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatDialog} from '@angular/material/dialog';
-import { MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule,MAT_DIALOG_DATA,
+  MatDialogTitle,
+  MatDialogContent,} from '@angular/material/dialog';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { AlfrescoApi } from '@alfresco/js-api';
+import { ContentModule } from '@alfresco/adf-content-services';
 export interface rekogData {
   src: string;
   name: string;
-  labels: string;
+  labels: [];
+  nodeid:string
 }
 
 @Component({
@@ -17,34 +21,46 @@ export interface rekogData {
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,AfterViewInit {
 
   ngOnInit(): void {
-
-    setTimeout(() => {
-      this.queryWebService();
-    },500)
-
+    this.queryWebService();
   }
 
+  ngAfterViewInit(): void {
+      /* setTimeout(() => {
+      this.queryWebService();
+    },50) */
+  }
   constructor(private http: HttpClient){}
   readonly dialog = inject(MatDialog);
   rkdata:any;
   longText = ``;
-  webserviceURL = 'http://127.0.0.1:5202/getrekognitionfiles'
+  showImages:boolean=true;
+  //this will automatically add the current hostname to the URL for the web service
+  webserviceURL = window.location.protocol+'//'+window.location.hostname +':5202/getrekognitionfiles'
 
   queryWebService(){
 
   this.http.get<rekogData>(this.webserviceURL).subscribe(data =>
       {
         this.rkdata = data;
+        this.showImages = true;
         console.log(this.rkdata);
       })
   }
-  openDialog() {
+  openDialog(i:any) {
+
+    console.log("i passed in -->" + i);
     const dialogRef = this.dialog.open(DialogContentExampleDialog,{
-      height:'90%',
-      width:'60%'
+      height:'100%',
+      width:'100%',
+      data: {
+        src: this.rkdata[i].src ,
+        nodeid: this.rkdata[i].src.split('.')[0],
+        name: this.rkdata[i].name,
+        labels: this.rkdata[i].labels
+      },
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -58,7 +74,11 @@ export class HomeComponent implements OnInit {
   styleUrls:['./home.component.scss'],
   templateUrl: './dialog-content-example-dialog.html',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule,MatGridListModule],
+  imports: [MatDialogModule, MatButtonModule,MatGridListModule,ContentModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DialogContentExampleDialog {}
+export class DialogContentExampleDialog {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: rekogData) {}
+
+}
