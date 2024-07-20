7c4a360f9992dd8544cf4fba6d7c564ce69b4ee1
Rob Wilds <robwilds@me.com>
2024-07-20 12:32:37 -0400

    added search in header. need to add redirect to search filter chips (or files)

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { NodeEntry } from "@alfresco/js-api";
+import { PreviewService } from "../services/preview.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app-layout.component.html',
-  styleUrls: ['./app-layout.component.scss']
+  selector: "app-root",
+  templateUrl: "./app-layout.component.html",
+  styleUrls: ["./app-layout.component.scss"],
 })
-export class AppLayoutComponent {}
+export class AppLayoutComponent {
+  constructor(public router: Router, private preview: PreviewService) {}
+
+  /**
+   * Called when the user submits the search, e.g. hits enter or clicks submit
+   *
+   * @param event Parameters relating to the search
+   */
+  onSearchSubmit(event: KeyboardEvent) {
+    const value = (event.target as HTMLInputElement).value;
+    this.router.navigate([
+      "/search",
+      {
+        q: value,
+      },
+    ]);
+  }
+
+  onItemClicked(event: NodeEntry) {
+    if (event.entry.isFile) {
+      this.preview.showResource(event.entry.id);
+    } else if (event.entry.isFolder) {
+      this.router.navigate(["/files", event.entry.id]);
+    }
+  }
+}
